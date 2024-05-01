9d774025ce54423fca118fd82a4b293034a05244
coed95 <codecoed95@gmail.com>
2024-05-01 17:21:45 +0200

    Import new css files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import "./css/contact.css";
+import "./css/content.css";
+import "./css/header.css";
+import "./css/home.css";
+import "./css/main.css";
+import "./css/menu.css";
 import "./css/style.css";
+
 import { Website } from "./website.js";
 
 Website.createContent();
